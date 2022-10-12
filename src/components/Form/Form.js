360ac5f29cdb5c00d6458e6ee4fa22d72b159d98
md311002 <95117014+md311002@users.noreply.github.com>
2022-10-12 18:18:21 +0530

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -42,7 +42,7 @@ const Form = ({ currentId, setCurrentId }) => {
 
     if (!user?.result?.name) {
         return (
-            <Paper className={classes.paper}>
+            <Paper className={classes.paper} elevation={6}>
                 <Typography variant='h6' align='center '>
                     Sign In to create post
                 </Typography>
